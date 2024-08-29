5e47eb4984a581d5ed91ff3fddf2b817d9c3b971
kenmasi14 <jankenzimasangkay@gmail.com>
2024-08-29 11:06:32 +0800

    vercel json added

--- backend/server.js
+++ backend/server.js
@@ -420,7 +420,7 @@ app.delete('/employees/:employeeId', async (req, res) => {
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
